ddb5baf6e208e648879ac6cfe96db4652dd4d9c3
Jawad alam <112615558+maksof-jawad@users.noreply.github.com>
2024-02-01 17:59:08 +0500

    migeration fix

--- database/migrations/2024_01_26_094219_template_add.php
+++ database/migrations/2024_01_26_094219_template_add.php
@@ -1,11 +1,11 @@
 <?php
 
+use BookStack\Users\Models\Template;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->boolean('isActive')->default(true);
             $table->timestamps();
         });
-        
+
         // update roles table
         Schema::table('roles', function (Blueprint $table) {
             $table->unsignedBigInteger('template_id')->nullable();
@@ -30,6 +30,17 @@ return new class extends Migration
                 ->on('template')
                 ->onDelete('set null');
         });
+        $templates = [
+            ['name' => 'Default', 'path' => 'home.templates.default', 'isActive' => true],
+            ['name' => 'Template 2', 'path' => 'home.templates.template_2', 'isActive' => true],
+            ['name' => 'Template 3', 'path' => 'home.templates.template_3', 'isActive' => true],
+        ];
+
+        // Insert each template into the template table
+        foreach ($templates as $templateData) {
+            Template::create($templateData);
+        }
+        DB::table('roles')->update(['template_id' => 1]);
     }
 
     /**
@@ -39,6 +50,18 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        // Rollback for updating the 'roles' table
+        Schema::table('roles', function (Blueprint $table) {
+            // Drop the foreign key constraint
+            $table->dropForeign(['template_id']);
+
+            // Remove the added column
+            $table->dropColumn('template_id');
+        });
+
+        // Rollback for creating the 'template' table
+        Schema::dropIfExists('template');
+
+
     }
 };
